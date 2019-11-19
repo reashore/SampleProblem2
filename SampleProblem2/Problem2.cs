de33353623fc8974815ac0a2350ade552ccd6b2f
Frank J. Reashore <reashore@telus.net>
2019-11-19 13:05:32 -0800

    Add project files.

---
+++ SampleProblem2/Problem2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace SampleProblem2
+{
+    public static class Problem2
+    {
+        public static int GeneralizedGreatestCommonDivisor(int[] array)
+        {
+            int arrayLength = array.Length;
+            List<int> gcdList = new List<int>();
+
+            for (int n = 0; n < arrayLength - 1; n++)
+            {
+                int gcd = GreatestCommonDivisor(array[n], array[n + 1]);
+                gcdList.Add(gcd);
+            }
+
+            int minGcd = 1;
+
+            foreach (int gcd in gcdList)
+            {
+                if (gcd > minGcd)
+                {
+                    minGcd = gcd;
+                }
+            }
+
+            return minGcd;
+        }
+
+        private static int GreatestCommonDivisor(int a, int b)
+        {
+            if (a < 0)
+            {
+                a = -a;
+            }
+
+            if (b < 0)
+            {
+                b = -b;
+            }
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                {
+                    a %= b;
+                }
+                else
+                {
+                    b %= a;
+                }
+            }
+
+            return a == 0 ? b : a;
+        }
+    }
+}
---
+++ SampleProblem2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SampleProblem2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] array = {2, 3, 4, 5, 6};
+            int gcd = Problem2.GeneralizedGreatestCommonDivisor(array);
+            Console.WriteLine($"gcd = {gcd}");
+
+            array = new [] {2, 4, 6, 8, 10};
+            gcd = Problem2.GeneralizedGreatestCommonDivisor(array);
+            Console.WriteLine($"gcd = {gcd}");
+        }
+    }
+}
